1b989ce7f716dfb483f2db8edf793523b246cdf9
Michael Ballard <ballard.michael9@gmail.com>
2018-05-08 22:19:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |char, index|
+   puts "#{index + 1}. #{char}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|x| x.capitalize! << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |x|
+    x.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
